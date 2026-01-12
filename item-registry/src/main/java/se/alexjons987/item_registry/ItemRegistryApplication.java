5981a649b3989192fa0195dceecdc0cf23fea6d2
alexjons987 <alexander.jonsson987@outlook.com>
2026-01-12 12:41:17 +0100

    Add initial spring app

---
+++ item-registry/src/main/java/se/alexjons987/item_registry/ItemRegistryApplication.java
@@ -0,0 +1,13 @@
+package se.alexjons987.item_registry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItemRegistryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ItemRegistryApplication.class, args);
+	}
+
+}
---
+++ item-registry/src/test/java/se/alexjons987/item_registry/ItemRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package se.alexjons987.item_registry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItemRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
