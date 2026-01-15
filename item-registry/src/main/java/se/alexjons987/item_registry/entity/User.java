48091c3f25ddb1c54183c6e7636f5eccea3fec9f
alexjons987 <alexander.jonsson987@outlook.com>
2026-01-15 13:23:29 +0100

    Add missing constructors and properties for User entity

--- item-registry/src/main/java/se/alexjons987/item_registry/entity/User.java
+++ item-registry/src/main/java/se/alexjons987/item_registry/entity/User.java
@@ -13,7 +13,56 @@ public class User {
 
     private String username;
     private String password;
+    private Long value;
 
     @OneToMany(mappedBy = "owner")
     private Set<Item> items;
+
+    public User() {
+    }
+
+    public User(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Long getValue() {
+        return value;
+    }
+
+    public void setValue(Long value) {
+        this.value = value;
+    }
+
+    public Set<Item> getItems() {
+        return items;
+    }
+
+    public void setItems(Set<Item> items) {
+        this.items = items;
+    }
 }
